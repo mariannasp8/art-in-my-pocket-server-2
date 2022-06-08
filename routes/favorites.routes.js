732b23181ce324fac40a0385732ed90f9fb14ad0
Mariana Pontes <marianasevinatep@gmail.com>
2022-06-08 12:59:08 +0100

    searchBar working

--- routes/favorites.routes.js
+++ routes/favorites.routes.js
@@ -8,28 +8,28 @@ const Collection = require("../models/Collection.model");
 // add pieces to the favorites
 
 // remove pieces from the collection
-router.put("/favorites/add-piece/:pieceId", (req, res) => {
+/* router.put("/favorites/add-piece/:pieceId", (req, res) => {
   console.log(req.params);
 
-  // Collection.findByIdAndUpdate(
-  //   collectionId,
-  //   { $pull: { pieces: pieceId } },
-  //   { new: true }
-  // )
+  Collection.findByIdAndUpdate(
+    collectionId,
+    { $pull: { pieces: pieceId } },
+    { new: true }
+  )
 
-  //   .then((updatedCollection) => res.status(201).json(updatedCollection))
-  //   .catch((err) => res.status(400).json({ message: "error" }));
-});
+    .then((updatedCollection) => res.status(201).json(updatedCollection))
+    .catch((err) => res.status(400).json({ message: "error" }));
+}); */
 
 router.put("/favorites/add-piece/:pieceId", (req, res) => {
   const { pieceId } = req.params;
   console.log(req.params);
-  // const { _id } = req.payload;
+  const { _id } = req.payload;
 
-  // User.findByIdAndUpdate(_id, { $push: { favorites: pieceId } }, { new: true })
+  User.findByIdAndUpdate(_id, { $push: { favorites: pieceId } }, { new: true })
 
-  //   .then((userFavorites) => res.status(201).json(userFavorites))
-  //   .catch((err) => res.status(400).json({ message: "error" }));
+    .then((userFavorites) => res.status(201).json(userFavorites))
+    .catch((err) => res.status(400).json({ message: "error" }));
 });
 
 // remove pieces from the favorites
